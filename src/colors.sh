b54b76ecfb3b47f0d8dbc731807c9e54ed68230e
sabrina titem <sabrina.titem@emploitic.com>
2024-02-17 12:37:45 +0100

    ``` feat : Color Picker for Bash Color Library
    The proposed feature aims to add an interactive color picker to the Bash Color Library. This color picker will be accessible via a command-line interface (CLI) tool, allowing users to visually select from the 256-color palette supported by modern terminals
    
    ```

--- src/colors.sh
+++ src/colors.sh
@@ -1,4 +1,11 @@
 # colors.sh
-RED="\e[31m"
-GREEN="\e[32m"
-ENDCOLOR="\e[0m"
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+MAGENTA='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+
+
--- src/print_colors.sh
+++ src/print_colors.sh
@@ -1,8 +1,18 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
-
+#source color
 source colors.sh
 
 # Print messages in different colors
-echo -e "${RED}This is some red text"
-echo -e "${GREEN}And this is some green text"
+echo -e "${RED}This is some red text${RESET}"
+echo -e "${GREEN}And this is some green text${RESET}"
+
+
+line_size=16
+
+for i in {0..255}; do
+    printf "\e[38;5;${i}m%3d\e[0m " "$i"
+    if (( (i + 1) % line_size == 0 )); then
+        echo  
+    fi
+done
